fccb589349bf16a9bfbd4c09704bde4dc04d8ed0
Abhiraj Aditya <abhiaditya860@gmail.com>
2023-07-19 21:22:00 +0530

    Add the toggle switch

--- website/views.py
+++ website/views.py
@@ -4,6 +4,7 @@ from .models import Note
 from . import db
 import json
 from website import ml_model
+from datetime import datetime
 
 views = Blueprint('views', __name__)
 
@@ -13,17 +14,23 @@ views = Blueprint('views', __name__)
 def home():
     if request.method == 'POST': 
         note = request.form.get('note')#Gets the note from the HTML 
-
+        switch_state = request.form.get('switchState')  # Retrieve the switch state
         if len(note) < 1:
             flash('Note is too short!', category='error') 
         else:
+            if switch_state:
+                # Check if there is a note for the current date in the database
+                current_date = datetime.now().date()
+                current_time = datetime.now().time()
+                latest_note = Note.query.filter_by(user_id=current_user.id, currDate=current_date).order_by(Note.currtime.desc()).first()
+
             # Compute sentiment using the nltk_vader_sentiment function
             sentiment, sentimentColor = ml_model.nltk_vader_sentiment(note)
             new_note = Note(data=note, user_id=current_user.id, sentiment=sentiment, sentimentColor = sentimentColor)
             db.session.add(new_note) #adding the note to the database 
             db.session.commit()
-            flash('Note added!', category='success')
-            return redirect(url_for('views.home'))  # Redirect to the home page to avoid form resubmission    
+            flash('Note added!', category='success')    
+        return redirect(url_for('views.home'))  # Redirect to the home page to avoid form resubmission    
     # Access the user's first name
     first_name = current_user.first_name
     
