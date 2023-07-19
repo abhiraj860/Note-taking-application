1a2d2910af51ba3a8a4c2ced7d5cb1c43c9fd87f
Abhiraj Aditya <abhiaditya860@gmail.com>
2023-07-19 16:50:52 +0530

    Change in the colors array

--- website/ml_model.py
+++ website/ml_model.py
@@ -7,28 +7,11 @@ app = Flask(__name__)
 
 def nltk_vader_sentiment(str):
         sentimentColors = [
-                "#FF0000",
-                "#8B0000",
-                "#B22222",
-                "#DC143C",
-                "#CD5C5C",
-                "#F08080",
-                "#FF6347",
-                "#FF7F50",
-                "#FF8C00",
-                "#FFA500",
-                "#FFD700",
-                "#F0E68C",
-                "#EEE8AA",
-                "#FFFF00",
-                "#FFFFE0",
-                "#FFFACD",
-                "#FFFFF0",
-                "#F0FFF0",
-                "#90EE90",
-                "#32CD32",
-                "#008000"
+                "#FF0000", "#8B0000", "#B22222", "#DC143C", "#CD5C5C", "#F08080", "#FF6347",
+                "#FF7F50", "#FF8C00", "#FFA500", "#FFD700", "#F0E68C", "#EEE8AA", "#FFFF00",
+                "#FFFFE0", "#FFFACD", "#FFFFF0", "#F0FFF0", "#90EE90", "#32CD32", "#008000"
         ]
+
 
         sid = SentimentIntensityAnalyzer()
         scores = sid.polarity_scores(str)
